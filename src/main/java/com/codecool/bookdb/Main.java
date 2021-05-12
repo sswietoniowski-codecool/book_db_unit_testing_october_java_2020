43da3a78c7b4a3ff5ddb4386db506621a8f58fd3
Sławomir Świętoniowski <83072528+sswietoniowski-codecool@users.noreply.github.com>
2021-05-12 16:19:12 +0200

    Fixes added.

--- src/main/java/com/codecool/bookdb/Main.java
+++ src/main/java/com/codecool/bookdb/Main.java
@@ -3,14 +3,19 @@ package com.codecool.bookdb;
 import com.codecool.bookdb.manager.BookDbManager;
 import com.codecool.bookdb.view.UserInterface;
 
+import java.util.logging.Logger;
+
 public class Main {
+    private static Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
         try
         {
             UserInterface ui = new UserInterface(System.in, System.out);
             new BookDbManager(ui).run();
         } catch (Exception exception) {
-            System.out.println(exception.getMessage());
+            System.out.println("Program error!");
+            logger.warning(exception.getMessage());
         }
     }
 }
